dad2f696094eb8799c503f7962c7f7865011c7f7
Anthony Bui <anthonybui@gmail.com>
2018-04-28 12:26:18 -0400

    Working keyboard presses

--- interface/interface.ino
+++ interface/interface.ino
@@ -1,20 +1,19 @@
-/* Encoder Library - NoInterrupts Example
-   http://www.pjrc.com/teensy/td_libs_Encoder.html
-
-   This example code is in the public domain.
-*/
-
 #define ENCODER_DO_NOT_USE_INTERRUPTS
 #include <Encoder.h>
 #include <Bounce2.h>
+#include <Keyboard.h>
+#include <Adafruit_NeoPixel.h>
 
-Encoder race(4, 5);
+Encoder race(8, 9);
 Encoder income(6, 7);
-Encoder education(2, 3);
+Encoder education(10, 11);
 
-String raceLabels[] = { "black", "hispanic", "other", "asian", "white" };
 String incomeLabels[] = { "lower", "middle", "upper-middle", "upper" };
+char incomeKeys[] = { 'Q', 'W', 'E', 'R' };
+String raceLabels[] = { "black", "hispanic", "other", "asian", "white" };
+char raceKeys[] = { 'A', 'S', 'D', 'F', 'G' };
 String educationLabels[] = { "less-than-high-school", "high-school", "some-college", "bachelors", "advanced" };
+char educationKeys[] = { 'Z', 'X', 'C', 'V', 'B' };
 
 long oldIncomePosition = -1;
 int incomeSelection = 0;
@@ -29,16 +28,29 @@ Bounce resetButton = Bounce();
 Bounce searchButton = Bounce();
 Bounce randomButton = Bounce();
 
-int searchButtonPin = 10;
-int searchLedPin = 11;
+int searchButtonPin = 12;
+int searchLedPin = 13;
 
-int resetButtonPin = 9;
-int resetLedPin = 8;
+int resetButtonPin = 2;
+int resetLedPin = 3;
 
-int randomButtonPin = 13;
-int randomLedPin = 12;
+int randomButtonPin = 5;
+int randomLedPin = 4;
+
+//const int dinPin = 2;    // Din pin to Arduino pin 4
+//const int numOfLeds = 48;
+
+//Adafruit_NeoPixel pixels = Adafruit_NeoPixel(numOfLeds, dinPin, NEO_GRB + NEO_KHZ800);
+
+int red = 255;    //Value from 0(led-off) to 255().
+int green = 0;
+int blue = 0;
+
+int incomingByte;
+int sceneNumber;
 
 void setup() {
+  Keyboard.begin();
   Serial.begin(115200);
   pinMode(resetButtonPin, INPUT_PULLUP);
   pinMode(searchButtonPin, INPUT_PULLUP);
@@ -56,7 +68,7 @@ void setup() {
 }
 
 void loop() {
-  //== Check all our encoders
+  //== Check all our encodersASSSSSSSSDDDDDDDDFFFFFFFFGGGGGGGGAAAAAAAASSSS
   checkIncomeEncoder();
   checkRaceEncoder();
   checkEducationEncoder();
@@ -65,6 +77,13 @@ void loop() {
   checkSearch();
   checkReset();
   checkRandom();
+
+//  lights();
+
+  if (Serial.available() > 0) {   // see if there's incoming serial data
+    incomingByte = Serial.read(); // read it
+    sceneNumber = incomingByte;
+  }
 
   //== global delay
   delay(1);
@@ -93,6 +112,8 @@ void checkIncomeEncoder() {
     incomeSelection = getEncoderSelection(incomePosition, 4);
     oldIncomePosition = incomePosition;
 
+
+    Keyboard.write(incomeKeys[incomeSelection]);
     Serial.print("income,");
     Serial.println(incomeLabels[incomeSelection]);
   }
@@ -105,6 +126,7 @@ void checkRaceEncoder() {
     raceSelection = getEncoderSelection(racePosition, 5);
     oldRacePosition = racePosition;
 
+    Keyboard.write(raceKeys[raceSelection]);
     Serial.print("race,");
     Serial.println(raceLabels[raceSelection]);
   }
@@ -117,18 +139,9 @@ void checkEducationEncoder() {
     educationSelection = getEncoderSelection(educationPosition, 5);
     oldEducationPosition = educationPosition;
 
+    Keyboard.write(educationKeys[educationSelection]);
     Serial.print("education,");
     Serial.println(educationLabels[educationSelection]);
-  }
-}
-
-void checkSearch() {
-  searchButton.update();
-
-  if (searchButton.fell()) {
-    digitalWrite(searchLedPin, HIGH);
-    Serial.print("button,");
-    Serial.println("search");
   }
 }
 
@@ -137,8 +150,20 @@ void checkReset() {
 
   if (resetButton.fell()) {
     digitalWrite(resetLedPin, HIGH);
+    Keyboard.write('I');
     Serial.print("button,");
     Serial.println("reset");
+  }
+}
+
+void checkSearch() {
+  searchButton.update();
+
+  if (searchButton.fell()) {
+    digitalWrite(searchLedPin, HIGH);
+    Keyboard.write('O');
+    Serial.print("button,");
+    Serial.println("search");
   }
 }
 
@@ -147,10 +172,8 @@ void checkRandom() {
 
   if (randomButton.fell()) {
     digitalWrite(randomLedPin, HIGH);
+    Keyboard.write('P');
     Serial.print("button,");
     Serial.println("random");
   }
 }
-
-
-
